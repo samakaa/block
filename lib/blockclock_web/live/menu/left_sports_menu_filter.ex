463c10c3fe509a926a7981c0053028fb32effc63
unknown <dallellwassim@gmail.com>
2023-03-03 02:51:49 +0100

    deploy final

--- lib/blockclock_web/live/menu/left_sports_menu_filter.ex
+++ lib/blockclock_web/live/menu/left_sports_menu_filter.ex
@@ -43,14 +43,14 @@ defmodule BlockclockWeb.LeftSporsMenuFilter do
          <a href="#"> Offers</a><i class="fa-solid fa-sack-dollar"></i></li>
 
        <h1>  FREQUENTLY VISITED</h1>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Football.png")} alt="Italian Trulli" ><a href = {Routes.pregame_path(@socket, :index, "soccer")}> Soccer</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/football.png")} alt="Italian Trulli" ><a href = {Routes.pregame_path(@socket, :index, "soccer")}> Soccer</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/ball.png")}><a href="#"> Baseball </a></div><button id="btn" class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/basketball.png")}><a href="#"> Basketball</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/boxing-gloves.png")} alt="Italian Trulli" ><a href="#"> Boxing/MMA</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
 
        <h1> FULL LIST</h1>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/rugby-ball.png")} alt="Italian Trulli" ><a href="#"> American Football</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Australian.png")} alt="Italian Trulli" ><a href="#"> Australian Rules</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/australian.png")} alt="Italian Trulli" ><a href="#"> Australian Rules</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/ball.png")} alt="Italian Trulli" ><a href="#"> Baseball</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/basketball.png")}><a href="#"> Basketball</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/boxing-gloves.png")} alt="Italian Trulli" ><a href="#">Boxing/MMA</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button> </li>
@@ -61,8 +61,8 @@ defmodule BlockclockWeb.LeftSporsMenuFilter do
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/gamepad.png")} alt="Italian Trulli" ><a href="#">Esports</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button> </li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/sports.png")} alt="Italian Trulli" ><a href="#">Fantasy Sports</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button> </li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/racing.png")} alt="Italian Trulli" ><a href="#">Formula 1</a> </div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Futsal.png")} alt="Italian Trulli" ><a href="#"> Futsal</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Gaelic-football.png")} alt="Italian Trulli" ><a href="#"> Gaelic Sports</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button> </li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/futsal.png")} alt="Italian Trulli" ><a href="#"> Futsal</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/gaelic-football.png")} alt="Italian Trulli" ><a href="#"> Gaelic Sports</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button> </li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/golf.png")} alt="Italian Trulli" ><a href="#">  Golf </a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/greyhound.png")} alt="Italian Trulli" ><a href="#"> Greyhounds </a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/hockey.png")} alt="Italian Trulli" ><a href="#">  Hockey</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
@@ -83,7 +83,7 @@ defmodule BlockclockWeb.LeftSporsMenuFilter do
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/professional.png")} alt="Italian Trulli" ><a href="#"> Rugby League</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/rugby.png")} alt="Italian Trulli" ><a href="#"> Rugby Union</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Snooker.png")} alt="Italian Trulli" ><a href="#"> Snookeer</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Football.png")} alt="Italian Trulli" ><a href = {Routes.pregame_path(@socket, :index, "soccer")}> Soccer</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/football.png")} alt="Italian Trulli" ><a href = {Routes.pregame_path(@socket, :index, "soccer")}> Soccer</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
 
        <li class="left-list-menu-list-2">
          <label for="btnn"  class="b-2"> Specials
@@ -101,7 +101,7 @@ defmodule BlockclockWeb.LeftSporsMenuFilter do
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/tennis.png")} alt="Italian Trulli" ><a href="#"> Tennis</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/horseback.png")} alt="Italian Trulli" ><a href="#"> Trotting</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
        <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/sport.png")} alt="Italian Trulli" ><a href="#"> Virtual Sports </a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
-       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/Voleyball.png")} alt="Italian Trulli" ><a href="#"> Volleyball</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
+       <li class="a"><div><img src={Routes.static_path(@socket,"/assets/images/sports-icon/voleyball.png")} alt="Italian Trulli" ><a href="#"> Volleyball</a></div><button id="btn"  class="bt"><i class="fa-regular fa-star"></i></button></li>
 
 
        <li class="left-list-menu-list-3">
