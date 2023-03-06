e9d5afabdd8726afa7b8b47c577a0146042b8c32
unknown <dallellwassim@gmail.com>
2023-03-06 01:05:47 +0100

    Prepare for deploy to Render

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Blockclock.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:httpoison, "~> 1.7"},
       {:poison, "~> 3.1"},
-      {:timex, "~> 3.7.9"},
+    #  {:timex, "~> 3.7.9"},
       {:surface, "~> 0.8.0"},
       {:phoenix_live_session, "~> 0.1"}
     ]
